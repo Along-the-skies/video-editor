6d812d612129feb280025dd6ce81b04a07209fdd
Vasudev <shajcheruvath@gmail.com>
2026-09-08 11:04:04 +0530

    placeholder fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const DOWNLOAD_URL = "#";
+const DOWNLOAD_URL = "https://github.com/Along-the-skies/video-editor/releases/download/Relases/VframeSetup.exe";
 
 document.getElementById('download-link').href = DOWNLOAD_URL;
 document.getElementById('download-link-2').href = DOWNLOAD_URL;
